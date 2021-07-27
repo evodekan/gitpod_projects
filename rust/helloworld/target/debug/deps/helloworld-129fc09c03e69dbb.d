35dfcafe1b9acea0134f71016dc1f2d337647cf3
evodekan <evodekanuma@gmail.com>
2021-07-27 09:38:16 +0000

    created helloworld rust project

---
+++ rust/helloworld/target/debug/deps/helloworld-129fc09c03e69dbb.d
@@ -0,0 +1,5 @@
+/home/gitpod/gitpod_projects/rust/helloworld/target/debug/deps/helloworld-129fc09c03e69dbb: src/main.rs
+
+/home/gitpod/gitpod_projects/rust/helloworld/target/debug/deps/helloworld-129fc09c03e69dbb.d: src/main.rs
+
+src/main.rs:
---
+++ rust/helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+/home/gitpod/gitpod_projects/rust/helloworld/target/debug/helloworld: /home/gitpod/gitpod_projects/rust/helloworld/src/main.rs
